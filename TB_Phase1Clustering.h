40e6fd0849edafd31d5bce2df5164cf8f751e054
Simone Bologna <smnbologna@gmail.com>
2019-03-04 12:44:52 +0000

    Added header

---
+++ TB_Phase1Clustering.h
@@ -0,0 +1,11 @@
+#ifndef __TB_PHASE1CLUSTERING_H__
+#define __TB_PHASE1CLUSTERING_H__
+
+#include "HLS_Phase1Clustering.h"
+
+void cleanGrid(CaloGrid grid, unsigned short int value=0);
+void cleanJets(Jets jets);
+void test9x9();
+void test5x5();
+
+#endif //__TB_PHASE1CLUSTERING_H__
