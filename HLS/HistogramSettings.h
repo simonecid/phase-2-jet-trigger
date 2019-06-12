4df0a221d03d9083ac031959e20adab8ccf74f53
Simone Bologna <smnbologna@gmail.com>
2019-06-12 14:40:08 +0100

    Prepared settings for final buffer

--- HLS/HistogramSettings.h
+++ HLS/HistogramSettings.h
@@ -1,6 +1,9 @@
 #ifndef __HISTOGRAMSETTINGS_H__
 #define __HISTOGRAMSETTINGS_H__
 
+#define ETA_GRID_SIZE 120
+#define PHI_GRID_SIZE 72
+
 #define ETA_BIN_SIZE 10
 #define PHI_BIN_SIZE 10
 #define PHI_N_BINS 8
@@ -9,13 +12,13 @@
 #define HG_ETA_N_BINS 6
 #define HF_ETA_N_BINS 6
 
-#define ETA_BIN_SIZE 10
-#define PHI_BIN_SIZE 10
-#define PHI_N_BINS 3
-#define BARREL_ETA_N_BINS 3
-#define TK_HG_ETA_N_BINS 3
-#define HG_ETA_N_BINS 3
-#define HF_ETA_N_BINS 3
+// #define ETA_BIN_SIZE 10
+// #define PHI_BIN_SIZE 10
+// #define PHI_N_BINS 3
+// #define BARREL_ETA_N_BINS 3
+// #define TK_HG_ETA_N_BINS 3
+// #define HG_ETA_N_BINS 3
+// #define HF_ETA_N_BINS 3
 
 #ifndef __SYNTHESIS__
 #define NUMBER_OF_INPUTS_PER_CLOCK 24
