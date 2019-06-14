5fee4f46200aebe2d26e4a924b9d3587b31b85fc
Simone Bologna <smnbologna@gmail.com>
2019-06-14 14:48:18 +0100

    Scaled down to barrel

--- HLS_HistogramBuffer.h
+++ HLS_HistogramBuffer.h
@@ -1,18 +1,19 @@
-#ifndef __HLS_HISTOGRAMMER_H__
-#define __HLS_HISTOGRAMMER_H__
+#ifndef __HLS_HISTOGRAM_BUFFER_H__
+#define __HLS_HISTOGRAM_BUFFER_H__
 
 #include "HLS/Types.h"
 #include "HLS/Histogram2D.h"
 #include "HLS/HistogramSettings.h"
+#include <hls_video.h>
 
-typedef hls::TPt TBuffer[PHI_N_BINS][ETA_GRID_SIZE];
+// typedef hls::TPt TBuffer[PHI_N_BINS][N_ETA_BINS_BARREL_REGION];
+typedef hls::Window<N_BINS_PHI_REGION * N_PHI_SEGMENTS, N_ETA_BINS_BARREL_REGION * N_ETA_SEGMENTS_BARREL, hls::TPt> TBarrelBuffer;
+
 
 void hls_histogram_buffer(
-                      const hls::Barrel_PfInputHistogram::TBins in_barrel_bins,
-                      const hls::TK_HG_PfInputHistogram::TBins in_tk_hg_bins,
-                      const hls::HG_PfInputHistogram::TBins in_hg_bins,
-                      const hls::HF_PfInputHistogram::TBins in_hf_bins,
-                      hls::TPt out_bins[ETA_GRID_SIZE]
+                      const hls::Barrel_PfInputHistogram::TBins inBarrelBins,
+                      hls::TPt outBins[N_ETA_BINS_BARREL_REGION * N_ETA_SEGMENTS_BARREL],
+                      bool reset
                      );
 
 #endif //__HLS_HISTOGRAMMER_H__
