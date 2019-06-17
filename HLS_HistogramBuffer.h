d9d74c8539fffbee9ddf7fc4e869595f54d12611
Simone Bologna <smnbologna@gmail.com>
2019-06-17 19:43:28 +0100

    Working towards whole detector integration

--- HLS_HistogramBuffer.h
+++ HLS_HistogramBuffer.h
@@ -7,7 +7,8 @@
 #include <hls_video.h>
 
 // typedef hls::TPt TBuffer[PHI_N_BINS][N_ETA_BINS_BARREL_REGION];
-typedef hls::Window<N_BINS_PHI_REGION * N_PHI_SEGMENTS, N_ETA_BINS_BARREL_REGION * N_ETA_SEGMENTS_BARREL, hls::TPt> TBarrelBuffer;
+typedef hls::Window<N_BINS_PHI, N_ETA_BINS_BARREL_REGION * N_ETA_SEGMENTS_BARREL, hls::TPt> TBarrelBuffer;
+typedef hls::Window<N_BINS_PHI, N_BINS_ETA, hls::TPt> TBuffer;
 
 
 void hls_histogram_buffer(
