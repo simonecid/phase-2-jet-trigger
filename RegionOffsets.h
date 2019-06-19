4905ec251c50c28eb8dbbeb834c0ecd163d60e5f
Simone Bologna <smnbologna@gmail.com>
2019-06-19 08:39:34 +0100

    Changed to include the whole detector

--- RegionOffsets.h
+++ RegionOffsets.h
@@ -6,371 +6,59 @@
 unsigned char returnBarrelEtaOffset(unsigned char regionId)
 {
   #pragma HLS inline 
-  const unsigned char barrelEtaOffsets[4] = {0, 9, 18, 27};
+  // const unsigned char barrelEtaOffsets[4] = {0, 9, 18, 27};
+  const unsigned char barrelEtaOffsets[36] = {42, 51, 60, 69, 42, 51, 60, 69, 42, 51, 60, 69, 42, 51, 60, 69, 42, 51, 60, 69, 42, 51, 60, 69, 42, 51, 60, 69, 42, 51, 60, 69, 42, 51, 60, 69};
   return barrelEtaOffsets[regionId];
-
-  // switch (regionId)
-  // {
-  //   case 0: return 0; break;
-  //   case 1: return 9; break;
-  //   case 2: return 18; break;
-  //   case 3: return 27; break;
-  //   case 0: return 42; break;
-  //   case 1: return 51; break;
-  //   case 2: return 60; break;
-  //   case 3: return 69; break;
-  //   case 4: return 42; break;
-  //   case 5: return 51; break;
-  //   case 6: return 60; break;
-  //   case 7: return 69; break;
-  //   case 8: return 42; break;
-  //   case 9: return 51; break;
-  //   case 10: return 60; break;
-  //   case 11: return 69; break;
-  //   case 12: return 42; break;
-  //   case 13: return 51; break;
-  //   case 14: return 60; break;
-  //   case 15: return 69; break;
-  //   case 16: return 42; break;
-  //   case 17: return 51; break;
-  //   case 18: return 60; break;
-  //   case 19: return 69; break;
-  //   case 20: return 42; break
-  //   case 21: return 51; break;
-  //   case 22: return 60; break;
-  //   case 23: return 69; break;
-  //   case 24: return 42; break;
-  //   case 25: return 51; break;
-  //   case 26: return 60; break;
-  //   case 27: return 69; break;
-  //   case 28: return 42; break;
-  //   case 29: return 51; break;
-  //   case 30: return 60; break;
-  //   case 31: return 69; break;
-  //   case 32: return 42; break;
-  //   case 33: return 51; break;
-  //   case 34: return 60; break;
-  //   case 35: return 69; break;
-  //   default: return 0; break;
-  // }
 }
 
 unsigned char returnBarrelPhiOffset(unsigned char &regionId)
 {
   #pragma HLS inline
-  const unsigned char barrelPhiOffsets[4] = {0, 0, 0, 0};
+  // const unsigned char barrelPhiOffsets[4] = {0, 0, 0, 0};
+  const unsigned char barrelPhiOffsets[36] = {0, 0, 0, 0, 8, 8, 8, 8, 16, 16, 16, 16, 24, 24, 24, 24, 32, 32, 32, 32, 40, 40, 40, 40, 48, 48, 48, 48, 56, 56, 56, 56, 64, 64, 64, 64};
   return barrelPhiOffsets[regionId];
-
-  // switch (regionId)
-  // {
-  //   case 0: return 0; break;
-  //   case 1: return 0; break;
-  //   case 2: return 0; break;
-  //   case 3: return 0; break;
-  //   case 4: return 8; break;
-  //   case 5: return 8; break;
-  //   case 6: return 8; break;
-  //   case 7: return 8; break;
-  //   case 8: return 16; break;
-  //   case 9: return 16; break;
-  //   case 10: return 16; break;
-  //   case 11: return 16; break;
-  //   case 12: return 24; break;
-  //   case 13: return 24; break;
-  //   case 14: return 24; break;
-  //   case 15: return 24; break;
-  //   case 16: return 32; break;
-  //   case 17: return 32; break;
-  //   case 18: return 32; break;
-  //   case 19: return 32; break;
-  //   case 20: return 40; break;
-  //   case 21: return 40; break;
-  //   case 22: return 40; break;
-  //   case 23: return 40; break;
-  //   case 24: return 48; break;
-  //   case 25: return 48; break;
-  //   case 26: return 48; break;
-  //   case 27: return 48; break;
-  //   case 28: return 56; break;
-  //   case 29: return 56; break;
-  //   case 30: return 56; break;
-  //   case 31: return 56; break;
-  //   case 32: return 64; break;
-  //   case 33: return 64; break;
-  //   case 34: return 64; break;
-  //   case 35: return 64; break;
-  //   default: return 0; break;
-  // }
 }
 
 unsigned char returnTKHGEtaOffset(unsigned char regionId)
 {
-  #pragma HLS inline off
-  switch (regionId)
-  {
-    case 0: return 36; break;
-    case 1: return 78; break;
-    case 2: return 36; break;
-    case 3: return 78; break;
-    case 4: return 36; break;
-    case 5: return 78; break;
-    case 6: return 36; break;
-    case 7: return 78; break;
-    case 8: return 36; break;
-    case 9: return 78; break;
-    case 10: return 36; break;
-    case 11: return 78; break;
-    case 12: return 36; break;
-    case 13: return 78; break;
-    case 14: return 36; break;
-    case 15: return 78; break;
-    case 16: return 36; break;
-    case 17: return 78; break;
-    // default: return 0; break;
-  }
+  #pragma HLS inline
+  const unsigned char tkhgEtaOffsets[18] = {36, 78, 36, 78, 36, 78, 36, 78, 36, 78, 36, 78, 36, 78, 36, 78, 36, 78};
+  return tkhgEtaOffsets[regionId];
 }
 
 unsigned char returnTKHGPhiOffset(unsigned char regionId)
 {
-  #pragma HLS inline off
-  switch (regionId)
-  {
-    case 0: return 0; break;
-    case 1: return 0; break;
-    case 2: return 8; break;
-    case 3: return 8; break;
-    case 4: return 16; break;
-    case 5: return 16; break;
-    case 6: return 24; break;
-    case 7: return 24; break;
-    case 8: return 32; break;
-    case 9: return 32; break;
-    case 10: return 40; break;
-    case 11: return 40; break;
-    case 12: return 48; break;
-    case 13: return 48; break;
-    case 14: return 56; break;
-    case 15: return 56; break;
-    case 16: return 64; break;
-    case 17: return 64; break;
-    // default: return 0; break;
-  }
+  #pragma HLS inline
+  const unsigned char tkhgPhiOffsets[18] = {0, 0, 8, 8, 16, 16, 24, 24, 32, 32, 40, 40, 48, 48, 56, 56, 64, 64};
+  return tkhgPhiOffsets[regionId];
 }
 
 unsigned char returnHGEtaOffset(unsigned char regionId)
 {
-  #pragma HLS inline off
-  switch (regionId)
-  {
-    case 0: return 24; break;
-    case 1: return 90; break;
-    case 2: return 24; break;
-    case 3: return 90; break;
-    case 4: return 24; break;
-    case 5: return 90; break;
-    case 6: return 24; break;
-    case 7: return 90; break;
-    case 8: return 24; break;
-    case 9: return 90; break;
-    case 10: return 24; break;
-    case 11: return 90; break;
-    case 12: return 24; break;
-    case 13: return 90; break;
-    case 14: return 24; break;
-    case 15: return 90; break;
-    case 16: return 24; break;
-    case 17: return 90; break;
-    // default: return 0; break;
-  }
+  #pragma HLS inline
+  const unsigned char hgEtaOffset[18] = {24, 90, 24, 90, 24, 90, 24, 90, 24, 90, 24, 90, 24, 90, 24, 90, 24, 90};
+  return hgEtaOffset[regionId];
 }
 
 unsigned char returnHGPhiOffset(unsigned char regionId)
 {
-  #pragma HLS inline off
-  switch (regionId)
-  {
-    case 0: return 0; break;
-    case 1: return 0; break;
-    case 2: return 8; break;
-    case 3: return 8; break;
-    case 4: return 16; break;
-    case 5: return 16; break;
-    case 6: return 24; break;
-    case 7: return 24; break;
-    case 8: return 32; break;
-    case 9: return 32; break;
-    case 10: return 40; break;
-    case 11: return 40; break;
-    case 12: return 48; break;
-    case 13: return 48; break;
-    case 14: return 56; break;
-    case 15: return 56; break;
-    case 16: return 64; break;
-    case 17: return 64; break;
-    // default: return 0; break;
-  }
+  #pragma HLS inline
+  const unsigned char hgPhiOffset[18] = {0, 0, 8, 8, 16, 16, 24, 24, 32, 32, 40, 40, 48, 48, 56, 56, 64, 64};
+  return hgPhiOffset[regionId];
 }
 
 unsigned char returnHFEtaOffset(unsigned char regionId)
 {
-  #pragma HLS inline off
-  switch (regionId)
-  {
-    case 0: return 0 ; break;
-    case 1: return 6 ; break;
-    case 2: return 12 ; break;
-    case 3: return 18 ; break;
-    case 4: return 96 ; break;
-    case 5: return 102 ; break;
-    case 6: return 108 ; break;
-    case 7: return 114 ; break;
-    case 8: return 0 ; break;
-    case 9: return 6 ; break;
-    case 10: return 12 ; break;
-    case 11: return 18 ; break;
-    case 12: return 96 ; break;
-    case 13: return 102 ; break;
-    case 14: return 108 ; break;
-    case 15: return 114 ; break;
-    case 16: return 0 ; break;
-    case 17: return 6 ; break;
-    case 18: return 12 ; break;
-    case 19: return 18 ; break;
-    case 20: return 96 ; break;
-    case 21: return 102 ; break;
-    case 22: return 108 ; break;
-    case 23: return 114 ; break;
-    case 24: return 0 ; break;
-    case 25: return 6 ; break;
-    case 26: return 12 ; break;
-    case 27: return 18 ; break;
-    case 28: return 96 ; break;
-    case 29: return 102 ; break;
-    case 30: return 108 ; break;
-    case 31: return 114 ; break;
-    case 32: return 0 ; break;
-    case 33: return 6 ; break;
-    case 34: return 12 ; break;
-    case 35: return 18 ; break;
-    case 36: return 96 ; break;
-    case 37: return 102 ; break;
-    case 38: return 108 ; break;
-    case 39: return 114 ; break;
-    case 40: return 0 ; break;
-    case 41: return 6 ; break;
-    case 42: return 12 ; break;
-    case 43: return 18 ; break;
-    case 44: return 96 ; break;
-    case 45: return 102 ; break;
-    case 46: return 108 ; break;
-    case 47: return 114 ; break;
-    case 48: return 0 ; break;
-    case 49: return 6 ; break;
-    case 50: return 12 ; break;
-    case 51: return 18 ; break;
-    case 52: return 96 ; break;
-    case 53: return 102 ; break;
-    case 54: return 108 ; break;
-    case 55: return 114 ; break;
-    case 56: return 0 ; break;
-    case 57: return 6 ; break;
-    case 58: return 12 ; break;
-    case 59: return 18 ; break;
-    case 60: return 96 ; break;
-    case 61: return 102 ; break;
-    case 62: return 108 ; break;
-    case 63: return 114 ; break;
-    case 64: return 0 ; break;
-    case 65: return 6 ; break;
-    case 66: return 12 ; break;
-    case 67: return 18 ; break;
-    case 68: return 96 ; break;
-    case 69: return 102 ; break;
-    case 70: return 108 ; break;
-    case 71: return 114 ; break;
-    // default: return 0; break;
-  }
+  #pragma HLS inline
+  const unsigned char hfEtaOffset[72] = {0, 6, 12, 18, 96, 102, 108, 114, 0, 6, 12, 18, 96, 102, 108, 114, 0, 6, 12, 18, 96, 102, 108, 114, 0, 6, 12, 18, 96, 102, 108, 114, 0, 6, 12, 18, 96, 102, 108, 114, 0, 6, 12, 18, 96, 102, 108, 114, 0, 6, 12, 18, 96, 102, 108, 114, 0, 6, 12, 18, 96, 102, 108, 114, 0, 6, 12, 18, 96, 102, 108, 114};
+  return hfEtaOffset[regionId];
 }
 
 unsigned char returnHFPhiOffset(unsigned char regionId)
 {
-  #pragma HLS inline off
-  switch (regionId)
-  {
-    case 0: return 0; break;
-    case 1: return 0; break;
-    case 2: return 0; break;
-    case 3: return 0; break;
-    case 4: return 0; break;
-    case 5: return 0; break;
-    case 6: return 0; break;
-    case 7: return 0; break;
-    case 8: return 8; break;
-    case 9: return 8; break;
-    case 10: return 8; break;
-    case 11: return 8; break;
-    case 12: return 8; break;
-    case 13: return 8; break;
-    case 14: return 8; break;
-    case 15: return 8; break;
-    case 16: return 16; break;
-    case 17: return 16; break;
-    case 18: return 16; break;
-    case 19: return 16; break;
-    case 20: return 16; break;
-    case 21: return 16; break;
-    case 22: return 16; break;
-    case 23: return 16; break;
-    case 24: return 24; break;
-    case 25: return 24; break;
-    case 26: return 24; break;
-    case 27: return 24; break;
-    case 28: return 24; break;
-    case 29: return 24; break;
-    case 30: return 24; break;
-    case 31: return 24; break;
-    case 32: return 32; break;
-    case 33: return 32; break;
-    case 34: return 32; break;
-    case 35: return 32; break;
-    case 36: return 32; break;
-    case 37: return 32; break;
-    case 38: return 32; break;
-    case 39: return 32; break;
-    case 40: return 40; break;
-    case 41: return 40; break;
-    case 42: return 40; break;
-    case 43: return 40; break;
-    case 44: return 40; break;
-    case 45: return 40; break;
-    case 46: return 40; break;
-    case 47: return 40; break;
-    case 48: return 48; break;
-    case 49: return 48; break;
-    case 50: return 48; break;
-    case 51: return 48; break;
-    case 52: return 48; break;
-    case 53: return 48; break;
-    case 54: return 48; break;
-    case 55: return 48; break;
-    case 56: return 56; break;
-    case 57: return 56; break;
-    case 58: return 56; break;
-    case 59: return 56; break;
-    case 60: return 56; break;
-    case 61: return 56; break;
-    case 62: return 56; break;
-    case 63: return 56; break;
-    case 64: return 64; break;
-    case 65: return 64; break;
-    case 66: return 64; break;
-    case 67: return 64; break;
-    case 68: return 64; break;
-    case 69: return 64; break;
-    case 70: return 64; break;
-    case 71: return 64; break;
-    // default: return 0; break;
-  }
+  #pragma HLS inline
+  const unsigned char hfPhiOffset[72] = {0, 0, 0, 0, 0, 0, 0, 0, 8, 8, 8, 8, 8, 8, 8, 8, 16, 16, 16, 16, 16, 16, 16, 16, 24, 24, 24, 24, 24, 24, 24, 24, 32, 32, 32, 32, 32, 32, 32, 32, 40, 40, 40, 40, 40, 40, 40, 40, 48, 48, 48, 48, 48, 48, 48, 48, 56, 56, 56, 56, 56, 56, 56, 56, 64, 64, 64, 64, 64, 64, 64, 64};
+  return hfPhiOffset[regionId];
 }
 
 void returnNextRegionOffset(unsigned char &etaOffset, unsigned char &phiOffset, bool reset)
