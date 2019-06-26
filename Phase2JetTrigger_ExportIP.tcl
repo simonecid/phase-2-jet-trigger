4aad875614f4e7bf1761ce54076fa207fd4ea5b4
Simone Bologna <smnbologna@gmail.com>
2019-06-26 18:07:24 +0100

    Fixed tcl

--- Phase2JetTrigger_ExportIP.tcl
+++ Phase2JetTrigger_ExportIP.tcl
@@ -30,6 +30,7 @@ set_part $part
 create_clock -period $clock
 #adding hls files
 add_files -cflags "-std=c++11" "HistogramAndBuffer/HLS_Histogrammer.cpp"
+add_files -cflags "-std=c++11" "HistogramAndBuffer/RegionOffsets.cpp"
 # adding testbench files
 add_files -cflags "-std=c++11" -tb "HistogramAndBuffer/TB_Histogrammer.cpp"
 # Sets the top-level function name that is going to be starting point for synthetisation
@@ -51,6 +52,7 @@ create_clock -period $clock
 add_files -cflags "-std=c++11" "HistogramAndBuffer/HLS_HistogramBuffer.cpp"
 # adding testbench files
 add_files -cflags "-std=c++11" -tb "HistogramAndBuffer/TB_HistogramBuffer.cpp"
+add_files -cflags "-std=c++11" "HistogramAndBuffer/RegionOffsets.cpp"
 # Sets the top-level function name that is going to be starting point for synthetisation
 set_top hls_histogram_buffer
 #synthetising
