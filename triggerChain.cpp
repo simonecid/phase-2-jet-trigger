6e849d785bad4062ec8f0b4f56fac72583d4bf04
Simone Bologna <smnbologna@gmail.com>
2019-06-26 17:14:22 +0100

    Tested chain for a single jet

--- triggerChain.cpp
+++ triggerChain.cpp
@@ -3,39 +3,47 @@
 #include "HistogramAndBuffer/HLS_HistogramBuffer.h"
 #include "HistogramAndBuffer/HLS/Types.h"
 
-#define ITERATIONS 10
+#include <assert.h>
+
+#define ITERATIONS 18
 
 typedef hls::TPt BarrelPhiSlice[N_ETA_BINS_BARREL_REGION * N_ETA_SEGMENTS_BARREL];
 
 int main(int argc, char const *argv[])
 {
   hls::Barrel_Inputs inputs[ITERATIONS];
-  hls::Barrel_Inputs::TBins barrel_bins[ITERATIONS];
+  hls::Barrel_PfInputHistogram::TBins barrel_bins[ITERATIONS];
   BarrelPhiSlice barrelPhiSlices[ITERATIONS];
+  Jets jets[ITERATIONS];
 
   for (unsigned int iteration = 0; iteration < ITERATIONS; iteration++)
   {
     for (unsigned int inputIdx = 0; inputIdx < NUMBER_OF_INPUTS_PER_CLOCK ; inputIdx++)
     {
-      inputs[inputIdx].pt = 0;
-      inputs[inputIdx].iPhi = 0;
-      inputs[inputIdx].iEta = 0;
+      inputs[iteration][inputIdx].pt = 0;
+      inputs[iteration][inputIdx].iPhi = 0;
+      inputs[iteration][inputIdx].iEta = 0;
     }
   }
 
-  inputs[0][inputIdx].pt = 100;
-  inputs[0][inputIdx].iPhi = 25;
-  inputs[0][inputIdx].iEta = 25;
+  inputs[1][3].pt = 100;
+  inputs[1][3].iPhi = 25;
+  inputs[1][3].iEta = 25;
 
-  for (unsigned int iteration = 0; iteration < ITERATIONS + ; iteration++)
+  for (unsigned int iteration = 0; iteration < ITERATIONS; iteration++)
   {
-    bool reset = (iteration == 0);
-    hls_histogrammer(inputs[iteration], barrel_bins[iteration], reset);
-    hls_histogram_buffer(barrel_bins[iteration], barrelPhiSlice[ITERATIONS], reset);
-    hls_jet_clustering()
+    const bool inReset = (iteration == 0);
+    bool outReset1, outReset2;
+    hls_histogrammer(inputs[iteration], barrel_bins[iteration], inReset, outReset1);
+    hls_histogram_buffer(barrel_bins[iteration], barrelPhiSlices[iteration], outReset1, outReset2);
+    hls_jet_clustering(barrelPhiSlices[iteration], jets[iteration], outReset2);
   }
 
-  
+  // std::raise(SIGINT);
+
+  assert(jets[5][11].pt == 100);
+  assert(jets[5][11].iEta == 11);
+  assert(jets[5][11].iPhi == 2);
 
   return 0;
 }
