4f29d9c9b0720dd883533e6ad052bc7b7ca8a836
Simone Bologna <smnbologna@gmail.com>
2019-06-28 12:32:16 +0100

    updated copy script

--- GetIPs.sh
+++ GetIPs.sh
@@ -2,8 +2,14 @@
 
 #utility script that takes the newly-produced ips and puts them together in a single folder
 
-mkdir -p IPs
+rm -r IPs/HLS_JetClustering
+rm -r IPs/HLS_Histogrammer
+rm -r IPs/HLS_HistogramBuffer
 
-cp HLS_JetClustering/KU115/impl/ip/cern-cms_hls_hls_jet_clustering_*.zip IPs/
-cp HLS_Histogrammer/KU115/impl/ip/cern-cms_hls_hls_histogrammer_*.zip IPs/
-cp HLS_HistogramBuffer/KU115/impl/ip/cern-cms_hls_hls_histogram_buffer_*.zip IPs/
+mkdir -p IPs/HLS_JetClustering/
+mkdir -p IPs/HLS_Histogrammer/
+mkdir -p IPs/HLS_HistogramBuffer/
+
+cp -r HLS_JetClustering/KU115/impl/ip/* IPs/HLS_JetClustering/
+cp -r HLS_Histogrammer/KU115/impl/ip/* IPs/HLS_Histogrammer/
+cp -r HLS_HistogramBuffer/KU115/impl/ip/* IPs/HLS_HistogramBuffer/
