8c6312f200c37a644d211b7c3e8c1eada94e8b17
Simone Bologna <smnbologna@gmail.com>
2019-07-01 08:33:09 +0100

    Removed reset port

--- JetClustering_setup.tcl
+++ JetClustering_setup.tcl
@@ -12,5 +12,8 @@ add_files -cflags "-std=c++11" "HLS_Phase1Clustering.cpp"
 # adding testbench files
 add_files -cflags "-std=c++11" -tb "TB_Phase1Clustering.cpp"
 
+# removing reset
+config_rtl -reset none
+
 # Sets the top-level function name that is going to be starting point for synthetisation
 set_top hls_jet_clustering
