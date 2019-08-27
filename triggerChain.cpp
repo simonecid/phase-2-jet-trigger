8bfbf006491d8aaf317148f49fe475d95a24d2ce
Simone Bologna <smnbologna@gmail.com>
2019-08-27 16:49:18 +0100

    Further testing

--- triggerChain.cpp
+++ triggerChain.cpp
@@ -5,7 +5,17 @@
 
 #include <assert.h>
 
-#define ITERATIONS RESET_PERIOD*2
+#define ITERATIONS RESET_PERIOD*3
+
+template<class TBinSlice>
+void printSlice(TBinSlice binSlice)
+{
+  for (unsigned int x = 0; x < N_ETA_BINS_BARREL_REGION; x++)
+  {
+    std::cout << binSlice[x] << " " ;
+  }
+  std::cout << std::endl;
+}
 
 typedef hls::TPt BarrelPhiSlice[N_ETA_BINS_BARREL_REGION * N_ETA_SEGMENTS_BARREL];
 
@@ -26,12 +36,12 @@ int main(int argc, char const *argv[])
     }
   }
 
-  inputs[0][3].pt = 100;
-  inputs[0][3].iPhi = 25;
-  inputs[0][3].iEta = 15;
+  inputs[RESET_PERIOD][3].pt = 100;
+  inputs[RESET_PERIOD][3].iPhi = 5;
+  inputs[RESET_PERIOD][3].iEta = 5;
   inputs[RESET_PERIOD + 1][3].pt = 100;
-  inputs[RESET_PERIOD + 1][3].iPhi = 25;
-  inputs[RESET_PERIOD + 1][3].iEta = 25;
+  inputs[RESET_PERIOD + 1][3].iPhi = 5;
+  inputs[RESET_PERIOD + 1][3].iEta = 5;
 
   bool inReset = true;
   bool outReset1, outReset2;
@@ -41,19 +51,21 @@ int main(int argc, char const *argv[])
 
   for (unsigned int iteration = 0; iteration < ITERATIONS; iteration++)
   {
+    std::cout << "Running cycle: " << iteration << ": \t";
     bool inReset = false;
     bool outReset1, outReset2;
+    // if (iteration == 24) std::raise(SIGINT);
     hls_histogrammer(inputs[iteration], barrel_bins[iteration], inReset, outReset1);
     hls_histogram_buffer(barrel_bins[iteration], barrelPhiSlices[iteration], outReset1, outReset2);
     hls_jet_clustering(barrelPhiSlices[iteration], jets[iteration], outReset2);
   }
 
-  assert(jets[5][1 / 2][1 % 2].pt == 100);
-  assert(jets[5][1 / 2][1 % 2].iEta == 1);
-  assert(jets[5][1 / 2][1 % 2].iPhi == 2);
-  assert(jets[5+RESET_PERIOD][11 / 2][11 % 2].pt == 100);
-  assert(jets[5+RESET_PERIOD][11 / 2][11 % 2].iEta == 11);
-  assert(jets[5+RESET_PERIOD][11 / 2][11 % 2].iPhi == 2);
+  assert(jets[11+RESET_PERIOD][0 / 2][0 % 2].pt == 100);
+  assert(jets[11+RESET_PERIOD][0 / 2][0 % 2].iEta == 0);
+  assert(jets[11+RESET_PERIOD][0 / 2][0 % 2].iPhi == 0);
+  assert(jets[11+RESET_PERIOD][9 / 2][9 % 2].pt == 100);
+  assert(jets[11+RESET_PERIOD][9 / 2][9 % 2].iEta == 9);
+  assert(jets[11+RESET_PERIOD][9 / 2][9 % 2].iPhi == 0);
 
   return 0;
 }
